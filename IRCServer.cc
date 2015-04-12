28350a4fa9f67c6f9f6ca679dd72ab39770e6b96
Evan Pagryzinski <epagryzi@purdue.edu>
2015-04-12 14:14:12 -0400

    Make IRCServer

--- IRCServer.cc
+++ IRCServer.cc
@@ -320,7 +320,7 @@ IRCServer::initialize()
     // Open password file
     fopen("passwords.txt", "a+");
     // Initialize users in room
-    referenceRoom = NULL;
+    referenceRoom = (roomStart*) malloc(sizeof(roomStart));
     fopen("open_rooms.txt", "w+");
 
     // Initalize message list
