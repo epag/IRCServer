cf64d020de5fbe3f4d0a47db5fe7c590fce75bad
Evan Pagryzinski <epagryzi@purdue.edu>
2015-04-12 14:30:32 -0400

    Make IRCServer

--- IRCServer.cc
+++ IRCServer.cc
@@ -390,6 +390,7 @@ IRCServer::enterRoom(int fd, const char * user, const char * password, const cha
         newRoom->roomName = strdup(args);
         newRoom->inRoom = n;
         write(fd, msg, strlen(msg));
+        newRoom->nextRoom = referenceRoom->start;
         referenceRoom->start = newRoom;
         return;
     }
