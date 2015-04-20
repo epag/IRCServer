7b88b5201e3a6b26da0d1518a14b4e137e77d77b
Evan Pagryzinski <epagryzi@purdue.edu>
2015-04-19 21:36:16 -0400

    Make IRCServer

--- test-server6.sh
+++ test-server6.sh
@@ -57,14 +57,14 @@ echo Send message
 ./TestIRCServer localhost $PORT "SEND-MESSAGE superman clarkkent java-programming message 1"
 ./TestIRCServer localhost $PORT "SEND-MESSAGE superman clarkkent java-programming message 2"
 ./TestIRCServer localhost $PORT "SEND-MESSAGE superman clarkkent java-programming message 3"
-./TestIRCServer localhost $PORT "SEND-MESSAGE superman clarkkent java-programming message 4"
-./TestIRCServer localhost $PORT "SEND-MESSAGE superman clarkkent java-programming message 5"
-./TestIRCServer localhost $PORT "SEND-MESSAGE superman clarkkent java-programming message 6"
-./TestIRCServer localhost $PORT "SEND-MESSAGE superman clarkkent java-programming message 7"
-./TestIRCServer localhost $PORT "SEND-MESSAGE superman clarkkent java-programming message 8"
+#./TestIRCServer localhost $PORT "SEND-MESSAGE superman clarkkent java-programming message 4"
+#./TestIRCServer localhost $PORT "SEND-MESSAGE superman clarkkent java-programming message 5"
+#./TestIRCServer localhost $PORT "SEND-MESSAGE superman clarkkent java-programming message 6"
+#./TestIRCServer localhost $PORT "SEND-MESSAGE superman clarkkent java-programming message 7"
+#./TestIRCServer localhost $PORT "SEND-MESSAGE superman clarkkent java-programming message 8"
 
-#echo Get messages from 0
-#./TestIRCServer localhost $PORT "GET-MESSAGES mary poppins 0 java-programming"
+echo Get messages from 0
+./TestIRCServer localhost $PORT "GET-MESSAGES mary poppins 0 java-programming"
 
 #echo Enter another user
 #./TestIRCServer localhost $PORT "LEAVE-ROOM mary poppins java-programming"
