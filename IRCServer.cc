4d043944b86d4b806232b6655bde077d25d212f8
Evan Pagryzinski <epagryzi@purdue.edu>
2015-04-21 11:52:04 -0400

    Make IRCServer

--- IRCServer.cc
+++ IRCServer.cc
@@ -629,7 +629,7 @@ IRCServer::getUsersInRoom(int fd, const char * user, const char * password, cons
     Chatter * n = r->inRoom;
 
     if (n == NULL) {
-        const char * newLine = "/r/n";
+        const char * newLine = "/n";
         write (fd, newLine, strlen(newLine));
         return;
     }
