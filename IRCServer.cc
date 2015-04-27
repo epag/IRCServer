d2825751380f89001886dbb6ef92f31b92a1ee03
Evan Pagryzinski <epagryzi@purdue.edu>
2015-04-27 08:30:06 -0400

    Make IRCServer

--- IRCServer.cc
+++ IRCServer.cc
@@ -853,7 +853,7 @@ IRCServer::getAllUsers2(int fd, const char * user, const char * password,const  
     }
 
 
-    for (int a = 0; a < i+1; a++) {
+    for (int a = 0; a < i; a++) {
         const char * newline = "*";
         write (fd, newline, strlen(newline));
         write (fd, NameHolder[a], strlen(NameHolder[a]));
